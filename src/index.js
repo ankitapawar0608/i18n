987a86a846f65772968666350df0ead6ccc6dcde
ankitapawar0608 <kedarpawar775@gmail.com>
2024-04-03 15:36:20 +0530

    internationaliazation complete

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from "./views/Home/Home"
 
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <Home/>
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,32 +1,54 @@
-import React, {  } from 'react'
+import React from 'react'
 import "./Home.css"
 import i18n from '../../utils/i18n';
 
 function Home() {
-     const users = 15;
+    // const users = 15;
 
-   
+
 
     return (
         <div>
-            <h1>{i18n("welcomemsg")}</h1>
 
-            <p>{i18n( "namemsg")}</p>
+            <div className="row d-flex justify-content-center">
+                <form className='form m-5 p-3 bg-white w-50 rounded'>
+                    <div className='m-4'>
 
-            <h3>{i18n( "greetingmsg")}</h3>
 
-            <select
-            defaultValue={localStorage.getItem("lang")}
-            onChange={(e)=>{
-                localStorage.setItem("lang",e.target.value);
-                window.location.reload();
-                 
-            }}>
-                <option value="en">English</option>
-                <option value="hi">Hindi</option>
-                <option value="mr">Marathi</option>
-            </select>
-            <p>{users} users are learning react.</p>
+
+                        <select
+                            defaultValue={localStorage.getItem("lang")}
+                            onChange={(e) => {
+                                localStorage.setItem("lang", e.target.value);
+                                window.location.reload();
+
+                            }}
+                            className=' form-control my-3 border-black'>
+                            <option value="en">English</option>
+                            <option value="hi">Hindi</option>
+                            <option value="mr">Marathi</option>
+                            <option value="sp">Spanish</option>
+                        </select>
+                        <h1>{i18n("welcomemsg")}</h1>
+
+                        <p className=''>{i18n("namemsg")}</p>
+
+                        <h3>{i18n("greetingmsg")}</h3>
+
+
+                    </div>
+                </form>
+            </div>
+
+
+
+
+
+
+
+
+
+            {/* <p>{users} users are learning react.</p> */}
         </div>
     )
 }
